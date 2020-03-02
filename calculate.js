f519680126db71547644f085e803df170b161aee
Eric <36748776+erwa4032@users.noreply.github.com>
2020-03-01 20:13:10 -0400

    project created

---
+++ calculate.js
@@ -0,0 +1,25 @@
+
+function d() {
+    var textarea1 = document.getElementById("r1");
+
+    let num = textarea1.value;
+
+
+
+    document.write("The area is " + twoDecimal(area(num)) + "<br>" + "The perimeter is " + twoDecimal(perimeter(num)));
+}
+
+
+
+function area(r) {
+    return Math.PI * Math.pow(r, 2);
+}
+
+function perimeter(r) {
+    return Math.PI * r * 2;
+}
+
+
+function twoDecimal(num) {
+    return Math.round(num * 100) / 100;
+}
